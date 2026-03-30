d4699f13916b705f322eb9bc89b2edefaea985a5
Alex Xiong <alexxiong655@gmail.com>
2026-03-30 14:06:30 -0500

    added more details for deck.ts, index.ts, App.tsx, etc.

---
+++ server/src/deck.ts
@@ -0,0 +1,28 @@
+import { Card, Suit, Value } from "../../shared/types";
+
+const SUITS: Suit[] = ["hearts", "diamonds", "clubs", "spades"];
+const VALUES: Value[] = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+
+export function createDeck(): Card[] {
+    return SUITS.flatMap(suit =>
+        VALUES.map(value => ({ suit, value }))
+    );
+}
+
+export function shuffleDeck(deck: Card[]): Card[] {
+    const shuffled = [...deck];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
+
+export function dealCards(numPlayers: number): Card[][] {
+    const deck = shuffleDeck(createDeck());
+    const hands: Card[][] = Array.from({ length: numPlayers }, () => []);
+    deck.forEach((card, i) => {
+        hands[i % numPlayers].push(card);
+    });
+    return hands;
+}
